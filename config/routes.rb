01d2198fe4fcbdad5ebfd23ab2378c45647aa410
lewis nyoike <nyoikelewis@gmail.com>
2018-10-04 07:59:46 +0300

    Configured the extra routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'home#index'
   resources :products
+  get 'contact_us', to: 'home#contact'
+  get 'about_us', to:'home#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
