2d48414aad783acf0a45253c8023264640952868
lewis nyoike <nyoikelewis@gmail.com>
2018-10-10 19:59:59 +0300

    Migrated the db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181009205342) do
+ActiveRecord::Schema.define(version: 20181010165519) do
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 20181009205342) do
     t.integer "image_file_size"
     t.datetime "image_updated_at"
     t.integer "category_id"
+    t.boolean "deal"
+    t.boolean "special_offer"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
