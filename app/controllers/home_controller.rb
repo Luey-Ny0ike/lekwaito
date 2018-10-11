98f602673b2ac1c4f6a94b4ab960c1821f0f7a0e
lewis nyoike <nyoikelewis@gmail.com>
2018-10-11 09:53:38 +0300

    Variables to call from products

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ActionController::Base
   def index
     @liquor = Product.last(3)
+    @deals = Product.where(deal: true)
+    @specialoffers = Product.where(special_offer: true)
+    @wc = Category.find_by_name("Whiskey")
+    @whiskey = Product.where(category_id: @wc.id)
+    @gc = Category.find_by_name("Gin")
+    @gin = Product.where(category_id: @gc.id)
   end
 end
