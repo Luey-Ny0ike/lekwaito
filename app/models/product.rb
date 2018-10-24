e3dd9c79036904fb3ab44f438a81f3a10fcfb76e
lewis nyoike <nyoikelewis@gmail.com>
2018-10-24 14:13:18 +0300

    After_save

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
   has_attached_file :image, styles: { large:"600x600>", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   belongs_to :category
+
+  after_save ThinkingSphinx::RealTime.callback_for(:product)
 end
