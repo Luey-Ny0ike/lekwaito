cf02f8cdbca4a0cec477e81a8740b7ed95a2d559
lewis nyoike <nyoikelewis@gmail.com>
2018-10-26 20:31:14 +0300

    Added the math methods and associations with order items

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   # RELATIONSHIPS
   belongs_to :category
-
+  has_one :order_item
   # PAPERCLIP ATTACHMENTS
   has_attached_file :image, styles: { large: '600x600>', medium: '300x300>', thumb: '100x100>' }, default_url: '/images/:style/missing.png'
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
@@ -13,5 +13,13 @@ class Product < ApplicationRecord
 
   def discounted_price
     price - offer_price
+  end
+
+  def unit_price
+    self.price - self.discounted_price
+  end
+
+  def total_price
+    order_item.quantity * unit_price
   end
 end
