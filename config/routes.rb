1f6cdcdc4f97293bac68d9db00ee22327e2eddfd
lewis nyoike <nyoikelewis@gmail.com>
2018-10-26 20:32:37 +0300

    Added the routes for the various pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
+  resources :deliveries
   resource :cart, only: [:show]
-  resources :order_items, only: [:create, :update, :destroy]
+  resources :order_items, only: %i[create update destroy]
 
   resources :categories
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-    devise_for :users, controllers: {
-     registrations: 'users/registrations'
-   }
+  get 'logout', to: 'devise/sessions#destroy'
+  devise_for :users,
+        controllers: {
+          registrations: 'users/registrations'
+        }
   root 'home#index'
   resources :products
   get 'contact_us', to: 'home#contact'
-  get 'about_us', to:'home#about'
+  get 'about_us', to: 'home#about'
+  get 'checkout', to: 'home#checkout'
+  get 'payment', to: 'home#payment'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
