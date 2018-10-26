c955ad9b6b1876afb73cec67a387946732a12e2f
lewis nyoike <nyoikelewis@gmail.com>
2018-10-26 03:10:26 +0300

    Defined a discounted method to use in the views

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,17 @@
 class Product < ApplicationRecord
-  has_attached_file :image, styles: { large:"600x600>", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  # RELATIONSHIPS
   belongs_to :category
 
+  # PAPERCLIP ATTACHMENTS
+  has_attached_file :image, styles: { large: '600x600>', medium: '300x300>', thumb: '100x100>' }, default_url: '/images/:style/missing.png'
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  # THINKING SPHINX SET UP
   after_save ThinkingSphinx::RealTime.callback_for(:product)
+
+  default_scope { where(active: true) }
+
+  def discounted_price
+    price - offer_price
+  end
 end
