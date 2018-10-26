84ff590f848057bbf43edf0dfd44791f709ddb8d
lewis nyoike <nyoikelewis@gmail.com>
2018-10-26 03:08:18 +0300

    Defined the current order method to utilize session block

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
   before_action :set_categorieso
+  helper_method :current_order
+
+def current_order
+  if !session[:order_id].nil?
+    Order.find(session[:order_id])
+  else
+    Order.new
+  end
+end
 
   private
   def set_categorieso
     @categorieso = Category.first(9)
+    @order_items = current_order.order_items
   end
 end
