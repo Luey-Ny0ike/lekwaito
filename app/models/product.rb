9f7fc8a23d4ba82a74337aa3b494d849521708ea
lewis nyoike <nyoikelewis@gmail.com>
2018-11-06 12:32:54 +0300

    Minor image bug fix

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ApplicationRecord
   belongs_to :category
   has_one :order_item
   # PAPERCLIP ATTACHMENTS
-  has_attached_file :image, styles: { large: '600x600>', medium: '300x300>', thumb: '100x100>' }, default_url: '/images/:style/missing.png'
+  has_attached_file :image, styles: { large: '600x600#', medium: '300x300#', thumb: '100x100#' }, default_url: '/images/:style/missing.png'
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   # THINKING SPHINX SET UP
